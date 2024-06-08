5a83104c6bb353411fde50fb3f87aea3866f3bc2
Greg <gregbailey617@gmal.com>
2024-06-08 18:08:04 -0400

    initial setup

--- models/index.js
+++ models/index.js
@@ -1,1 +1,23 @@
-// Added code 
+const User = require('./User');
+// const Post = require('./Post');
+
+
+
+module.exports = { User };
+
+
+
+
+/*
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+
+*/
